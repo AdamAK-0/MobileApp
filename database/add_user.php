4caeeeeed80d43c6b66365d848cbe7ab74cebdb6
AdamAK-0 <adamabdelkarim123@gmail.com>
2025-11-25 13:38:39 +0200

    php

--- database/add_user.php
+++ database/add_user.php
@@ -1,21 +1,32 @@
 <?php
 require_once 'connection.php';
 
-$first = $_POST['first_name'];
-$middle = $_POST['middle_name'];
-$last = $_POST['last_name'];
-$birth = $_POST['birth_year'];
-$email = $_POST['email'];
+$first   = $_POST['first_name'];
+$middle  = $_POST['middle_name'];
+$last    = $_POST['last_name'];
+$birth   = $_POST['birth_year'];
+$email   = $_POST['email'];
 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-// Insert user
 $query = "INSERT INTO users (first_name, middle_name, last_name, birth_year, email, password)
           VALUES ('$first', '$middle', '$last', '$birth', '$email', '$password')";
 
 if (mysqli_query($con, $query)) {
-    $user_id = mysqli_insert_id($con); // Get the last inserted ID
-    echo json_encode(["status" => "success", "user_id" => $user_id]);
+    $user_id = mysqli_insert_id($con);
+
+    // Fetch the full user row
+    $userQuery = "SELECT * FROM users WHERE user_id = $user_id";
+    $result = mysqli_query($con, $userQuery);
+    $user = mysqli_fetch_assoc($result);
+
+    echo json_encode([
+        "status" => "success",
+        "user" => $user
+    ]);
 } else {
-    echo json_encode(["status" => "fail", "error" => mysqli_error($con)]);
+    echo json_encode([
+        "status" => "fail",
+        "error" => mysqli_error($con)
+    ]);
 }
 ?>
