935426fa5feeedd34516d7973c8d340a40f5fd80
AdamAK-0 <adamabdelkarim123@gmail.com>
2025-11-27 07:47:58 +0200

    fixed pic issue, some fields in interbships

--- App/app/src/main/java/com/lau/portin/Internship.java
+++ App/app/src/main/java/com/lau/portin/Internship.java
@@ -5,13 +5,20 @@ import java.io.Serializable;
 public class Internship implements Serializable {
     public int id;
     public String companyName;
+    public int company_id;
     public String name;
     public String description;
     public String type;
     public String photo;
+    public int rating;
+    public String startDate;
+    public String endDate;
+    public int maxSlots;
+    public int slots;
 
     public Internship(int id, int company_id, String companyName, String name, String description, String type, String photo) {
         this.id = id;
+        this.company_id = company_id;
         this.companyName = companyName;
         this.name = name;
         this.description = description;
@@ -37,5 +44,23 @@ public class Internship implements Serializable {
     }
     public String getType() {
         return type;
+    }
+    public int getRating() {
+        return rating;
+    }
+    public String getStartDate() {
+        return startDate;
+    }
+    public String getEndDate() {
+        return endDate;
+    }
+    public int getMaxSlots() {
+        return maxSlots;
+    }
+    public int getSlots() {
+        return slots;
+    }
+    public int getCompany_id() {
+        return company_id;
     }
 }
